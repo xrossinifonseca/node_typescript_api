4142175d3f488df73fb955fbe53ca5dda33d32b0
Rossini Fonseca <rossinidrum@outlook.com>
2023-04-21 11:19:06 -0300

    adiciona condicional inputs createStore

--- src/controllers/store-controller.ts
+++ src/controllers/store-controller.ts
@@ -3,13 +3,16 @@ import { StoreRepository } from "../repositories/store-repository";
 export class StoreController {
   public async handle(req: Request, res: Response) {
     const newStore = new StoreRepository();
+    const { name, region } = req.body;
 
     try {
+      if (!name || !region) res.status(404).send("Input request required");
+
       const result = await newStore.createStore(req.body);
 
-      return res.json(result);
+      return res.send(result);
     } catch (err) {
-      return res.status(404).json({ error: err });
+      res.status(404).send({ err: `Store already exists` });
     }
   }
 }
