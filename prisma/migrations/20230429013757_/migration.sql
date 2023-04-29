197ca08e1052a6e97fe133a991143eb9295fc3c3
Rossini Fonseca <rossinidrum@outlook.com>
2023-04-29 11:02:17 -0300

    edita coluna table stock

---
+++ prisma/migrations/20230429013757_/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `stores` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `region` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Stock` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `serieNumber` VARCHAR(191) NOT NULL,
+    `qty` INTEGER NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `validity` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
