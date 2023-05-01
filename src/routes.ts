53d6a09ec6d5f332a29ca78a373e365dfe235b6d
Rossini Fonseca <rossinidrum@outlook.com>
2023-04-30 22:38:34 -0300

    adiciona rota post /stock

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { StoreController } from "./controllers/store-controller";
+import { StockController } from "./controllers/stock-controller";
 
 const router = Router();
 
 router.post("/stores", (req, res) => {
   new StoreController().handle(req, res);
+});
+
+router.post("/stock", (req, res) => {
+  new StockController().handle(req, res);
 });
 
 export { router };
