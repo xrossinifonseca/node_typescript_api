5e4434f5c441499cab58ebd647c348498c8fa096
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-17 20:04:48 -0300

    adiciona endpoint PUT /products/:id

--- src/routes.ts
+++ src/routes.ts
@@ -5,15 +5,20 @@ import { productController } from "./utils/dependencies";
 const router = Router();
 const storeRouteController = new StoreController();
 
-router.post("/stores", (req, res) => storeRouteController.handle(req, res));
+router.post("/stores", storeRouteController.handle);
 
-router.post("/product", productController.handle.bind(productController));
+router.post("/product", productController.postProduct.bind(productController));
 
 router.get("/product", productController.getAllProduct.bind(productController));
 
 router.get(
   "/product/:name",
   productController.getProductSelected.bind(productController)
+);
+
+router.put(
+  "/product/:id",
+  productController.putProduct.bind(productController)
 );
 
 export { router };
