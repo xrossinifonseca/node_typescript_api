6149cb431fc0df90f4feac8b22249e4fa91f4fdb
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-23 20:10:16 -0300

    refatora e adiciona estrutura de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 import { Server } from "./app";
+import { ProductRouter } from "./routes/product-routes";
+import { productDependecy } from "./dependencies/productDependencies";
+
 const server = new Server();
+const productRoute = new ProductRouter(productDependecy);
+
+server.app.use("/products", productRoute.getRouter());
 
 server.start();
