8daaebd2a47578d96f5ae0d63568bd702687d584
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-26 15:59:37 -0300

    adiciona teste productEntry

---
+++ src/repositories/stock/stock-repository.test.ts
@@ -0,0 +1,47 @@
+import { afterAll, beforeEach, describe, expect, it } from "vitest";
+import { prismaTest } from "../../infra/database/prismaTestClient";
+import { ProductRepository } from "../product-repository";
+import { StockRepository } from "./stock-repository";
+import { ProductEntity } from "../../entities/Product";
+import { Decimal } from "@prisma/client/runtime";
+import { StockEntity } from "../../entities/StockEntity";
+
+describe("StockRepository", () => {
+  const productRepository = new ProductRepository(prismaTest);
+  const stockRepository = new StockRepository(prismaTest);
+
+  const product: ProductEntity = {
+    name: "test",
+    price: new Decimal(10),
+    lotNumber: "123",
+    qty: 10,
+    validity: new Date(),
+  };
+
+  beforeEach(async () => {
+    await prismaTest.stock.deleteMany();
+    await prismaTest.product.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prismaTest.stock.deleteMany();
+    await prismaTest.product.deleteMany();
+    await prismaTest.$disconnect();
+  });
+
+  describe("productEntry", () => {
+    it("must register the entry of product into the stock", async () => {
+      const newProduct = await productRepository.registerProduct(product);
+
+      const newEntry: StockEntity = {
+        productId: newProduct.id,
+        quantity: 5,
+      };
+      const entry = await stockRepository.productEntry(newEntry);
+
+      expect(entry.productId).toEqual(newProduct.id);
+      expect(entry.quantity).toBe(5);
+      expect(entry).toHaveProperty("id");
+    });
+  });
+});
