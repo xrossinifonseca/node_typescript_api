41564f2c75d9ed8aa552dceaf8dbd94e63c6f8f7
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-29 16:29:19 -0300

    adiciona metodos getByProductId e  updateProducInStock

--- src/repositories/stock/stock-repository.ts
+++ src/repositories/stock/stock-repository.ts
@@ -1,22 +1,25 @@
 import { PrismaClient, Stock } from "@prisma/client";
 import { StockEntity } from "../../entities/StockEntity";
+import { ProductRepository } from "../product-repository";
 
 export class StockRepository {
   private prismaClient: PrismaClient;
+  productRepository: ProductRepository;
 
   constructor(prismaClient: PrismaClient) {
     this.prismaClient = prismaClient;
+    this.productRepository = new ProductRepository(this.prismaClient);
   }
 
   public async productEntry(product: StockEntity): Promise<Stock> {
-    const newProduct = await this.prismaClient.stock.create({
+    const newEntry = await this.prismaClient.stock.create({
       data: {
         product: { connect: { id: product.productId } },
         quantity: product.quantity,
       },
     });
 
-    return newProduct;
+    return newEntry;
   }
 
   public async productsInStock(): Promise<Stock[]> {
@@ -25,5 +28,33 @@ export class StockRepository {
     });
 
     return products;
+  }
+
+  public async getByProductId(id: string): Promise<Stock | null> {
+    const product = await this.prismaClient.stock.findFirst({
+      where: {
+        product: {
+          id: id,
+        },
+      },
+    });
+
+    return product;
+  }
+
+  public async updateProductInStock(
+    product: Stock,
+    quantity: number
+  ): Promise<Stock> {
+    const updated = await this.prismaClient.stock.update({
+      where: {
+        id: product.id,
+      },
+      data: {
+        quantity: quantity,
+      },
+    });
+
+    return updated;
   }
 }
