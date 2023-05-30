9cdc135ae949eb029eb4f3b281c93af4d6d1c006
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-30 11:32:29 -0300

    cria classe StockController

---
+++ src/controllers/stock/stock-controller.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { StockService } from "../../services/stock/stock-service";
+import { StockEntity } from "../../entities/StockEntity";
+import { handleError } from "../../utils/handleError";
+
+export class StockController {
+  private stockService: StockService;
+
+  constructor(stockService: StockService) {
+    this.stockService = stockService;
+  }
+
+  async postEntry(req: Request, res: Response): Promise<void> {
+    try {
+      const data: StockEntity = req.body;
+
+      const newEntry = await this.stockService.entrySafely(data);
+
+      res.status(201).send({
+        message: "Product entry saved successfully",
+        newEntry,
+      });
+    } catch (err: unknown) {
+      handleError(err, res);
+    } finally {
+      res.end();
+    }
+  }
+
+  async getProductsInStock(req: Request, res: Response): Promise<void> {
+    try {
+      const products = await this.stockService.productsInStockSafely();
+
+      res.status(200).send({
+        products,
+      });
+    } catch (err: unknown) {
+      handleError(err, res);
+    } finally {
+      res.end();
+    }
+  }
+
+  async getByProductId(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+
+      const product = await this.stockService.findByProductIdSefaly(id);
+
+      res.status(200).send({ product });
+    } catch (err: unknown) {
+      handleError(err, res);
+    } finally {
+      res.end();
+    }
+  }
+}
