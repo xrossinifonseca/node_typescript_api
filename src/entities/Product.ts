fa1531dc33ab4ee43d96ebc60e5f3e28c7aa5d7e
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-30 15:42:07 -0300

    modifica interface ProductEntity

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,10 +1,7 @@
 import { Prisma } from "@prisma/client";
 export interface ProductEntity {
   name: string;
-  lotNumber: string;
-  qty: number;
-  price: Prisma.Decimal;
-  validity?: Date | null;
+  price: number;
 }
 
 export interface DeleteProductResponse {
