d4ab2490c12edbf01a48c76f7ca30fe4f70ce3f9
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-30 11:32:59 -0300

    cria classe StockRoutes

---
+++ src/routes/stock-routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { StockController } from "../controllers/stock/stock-controller";
+
+export class StockRouter {
+  private stockController: StockController;
+  private router: Router;
+
+  constructor(stockController: StockController) {
+    this.stockController = stockController;
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(
+      "/",
+      this.stockController.postEntry.bind(this.stockController)
+    );
+    this.router.get(
+      "/",
+      this.stockController.getProductsInStock.bind(this.stockController)
+    );
+    this.router.get(
+      "/:id",
+      this.stockController.getByProductId.bind(this.stockController)
+    );
+  }
+
+  public getRouter(): Router {
+    return this.router;
+  }
+}
