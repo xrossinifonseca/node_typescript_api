223e3fb9aca0d9cf5ad43f684d01c2edda542c48
Rossini Fonseca <rossinidrum@outlook.com>
2023-05-30 11:34:24 -0300

    adiciona novo endpoint /stock

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { Server } from "./app";
 import { ProductRouter } from "./routes/product-routes";
 import { productDependecy } from "./dependencies/productDependencies";
+import { StockRouter } from "./routes/stock-routes";
+import { stockDepedenncy } from "./dependencies/stock-depedency/stockDependencies";
 
 const server = new Server();
 const productRoute = new ProductRouter(productDependecy);
+const stockRoute = new StockRouter(stockDepedenncy);
 
 server.app.use("/products", productRoute.getRouter());
+server.app.use("/stock", stockRoute.getRouter());
 
 server.start();
